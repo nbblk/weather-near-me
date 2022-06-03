8ecc2f2dce9da210a4d977754285a00f87048947
blake <blake@infrablockchain.com>
2022-06-03 10:20:28 +0900

    config: use @ prefix to replace relative paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,50 +2,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 import * as path from 'path';
-import { fileURLToPath } from 'url';
 
 const dirname = path.resolve(__dirname, './src');
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      '@': path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'src'),
-    },
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(dirname),
+      },
+      {
+        find: '@features',
+        replacement: path.resolve(dirname, '/features'),
+      },
+    ],
   },
-  // resolve: {
-  //   alias: {
-  //     '@': path.resolve(__dirname, './src'),
-  //   },
-  // },
-  // resolve: {
-  //   alias: [
-  //     { find: '@App', replacement: path.resolve(dirname, '/app') },
-  //     {
-  //       find: '@Features',
-  //       replacement: path.resolve(dirname, 'features'),
-  //     },
-  //     { find: '@Pages', replacement: path.resolve(dirname, '/pages') },
-  //     {
-  //       find: '@APIs',
-  //       replacement: path.resolve(dirname, '/common/apis'),
-  //     },
-  //     {
-  //       find: '@Assets',
-  //       replacement: path.resolve(dirname, '/common/assets'),
-  //     },
-  //     {
-  //       find: '@Components',
-  //       replacement: path.resolve(dirname, '/common/components'),
-  //     },
-  //     {
-  //       find: '@Hooks',
-  //       replacement: path.resolve(dirname, '/common/hooks'),
-  //     },
-  //     {
-  //       find: '@Utils',
-  //       replacement: path.resolve(dirname, '/common/utils'),
-  //     },
-  //   ],
-  // },
 });
