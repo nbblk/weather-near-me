32476d0061c7561a2e59595ca0c737d16e95b5a5
blake <blake@infrablockchain.com>
2022-06-09 17:14:19 +0900

    edit config to use @/

--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,19 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { resolve } from 'path';
 
-const dirname = path.resolve(__dirname, './src');
+//const dirname = path.resolve(__dirname, './src');
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: [
-      // {
-      //   find: /^@/,
-      //   replacement: path.resolve(dirname),
-      // },
-      // {
-      //   find: '@',
-      //   replacement: path.resolve(dirname),
-      // },
-      // {
-      //   find: '@features',
-      //   replacement: path.resolve(dirname, '/features'),
-      // },
-    ],
-  },
+ resolve: {
+   alias: [
+     {
+       find: '@',
+       replacement: resolve('/src')
+     }
+   ]
+ },
 });
