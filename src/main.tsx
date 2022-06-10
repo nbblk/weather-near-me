00d547ec9d2654e16ad6a1b6b12cef462ebf3cb5
blake <blake@infrablockchain.com>
2022-06-10 09:54:43 +0900

    change .js to rc

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 import App from './app/App';
 import './index.css';
 import store from './app/store';
 import { Provider } from 'react-redux';
-
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
-        </BrowserRouter>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 );
