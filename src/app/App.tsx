f8a99f8e90c0a50af984be6e0f9724d55cdc024b
blake <blake@infrablockchain.com>
2022-06-10 17:08:47 +0900

    wip

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,16 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 import Header from '@/common/components/organisms/Header/Header';
-import Summary from "@/pages/Summary";
-import Map from "@/pages/Map";
+import Summary from '@/pages/Summary';
+import Map from '@/pages/Map';
 
 import './App.css';
 
 function App() {
-
   return (
     <div className="App">
       <Header />
       <Routes>
+        <Route path="/" element={<Summary />}></Route>
         <Route path="/summary" element={<Summary />}></Route>
         <Route path="/map" element={<Map />}></Route>
       </Routes>
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-import * as path from 'path';
-import { fileURLToPath } from 'url';
 import { resolve } from 'path';
 
 //const dirname = path.resolve(__dirname, './src');
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
- resolve: {
-   alias: [
-     {
-       find: '@',
-       replacement: resolve('/src')
-     }
-   ]
- },
+  define: {
+    'process.env': process.env,
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: resolve('/src'),
+      },
+    ],
+  },
 });
