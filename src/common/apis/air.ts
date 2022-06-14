85d492015058157bac19dcd48d430bcbe341558e
nbblks <nbblks@gmail.com>
2022-06-15 06:50:20 +0900

    remove unused files

--- src/common/apis/air.ts
+++
@@ -1,6 +0,0 @@
-import 'dotenv/config';
-
-export const getAirPollutionBySidoUrl = (sidoName: string) => {
-  // 서울
-  return `http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?sidoName=${sidoName}&pageNo=1&numOfRows=100&returnType=json&serviceKey=${process.env.AIR_CERT_KEY}&ver=1.0`;
-};
